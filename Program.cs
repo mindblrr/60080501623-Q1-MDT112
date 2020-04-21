2a607541f2d178eb3b60b5200b4e87d0a2a72e0f
ASUS <ASUS@LAPTOP-LTCJOUID>
2020-04-22 01:46:49 +0700

    1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            void setup()
+            {
+                Serial.begin(9600);  
+                pinMode(2, INPUT_PULLUP);
+                pinMode(3, OUTPUT);
+                pinMode(4, OUTPUT);
+                pinMode(5, OUTPUT);
+                pinMode(6, OUTPUT);
+                pinMode(7, OUTPUT);
+                pinMode(9, OUTPUT);
+                pinMode(10, OUTPUT);
+                pinMode(11, OUTPUT);
+                pinMode(12, OUTPUT);
+                pinMode(13, OUTPUT);
+                tone(8, 200, 500);  
+                delay(250);        
+                tone(8, 400, 100);    
+            }
         }
     }
 }
